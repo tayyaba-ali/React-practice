f24a42636fa77c2b5f68b25a6b811a3754128a9b
Tayyaba-Ali20 <ta6402792@gmail.com>
2023-01-31 01:22:41 +0500

    You have to use className instead of class either in JSX or in React manually making components

--- src/components/WithoutJsx.jsx
+++ src/components/WithoutJsx.jsx
@@ -10,15 +10,14 @@ const Hello = () => {
     // The React.createElement takes 3 parameters first HTMl tag second attributes or extra information like classes or id and third dom node like if you want h1 inside a div having the text "Hello I have been created without using JSx" 
 
     return React.createElement(
-			'div',
-			null,
+			'div',// React not giving us permission to use the class keyword as it is reserved in HTMl and Js so instead of class use className
+			{className:"App-link"}, // For giving attributes you have to mention it in Object key value pair like this bhot matha khore bhae 
 			React.createElement(
 				'h1',
 				null,
 				'Hello I am here without using the JSX very painful yaar  bhot zyada coding horhe ha Toba Toba tum JSX he use krlo Allah ka Wasta🙂',
-			),
-		);
-    
+			)
+    )
     
 
 };
