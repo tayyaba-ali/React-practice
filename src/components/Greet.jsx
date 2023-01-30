1e80e743afd8ab63fb04e0278393f4fbb9e0297b
Tayyaba-Ali20 <ta6402792@gmail.com>
2023-01-30 21:23:55 +0500

    another component

---
+++ src/components/Greet.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+ const Greet = () => <h1>Hello Tayyaba😂</h1>
+
+export default Greet;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+       <h1>Hello I am Header</h1>
+    </div>
+  )
+}
